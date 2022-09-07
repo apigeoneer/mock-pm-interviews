d1e5f031ee35ce8712f09b28797dbd6fd259c061
Chitranshi <apigeoneer@gmail.com>
2022-09-07 12:57:41 +0530

    chore: creating a .env file and connecting to mongoose

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, () => {
+    console.log("MongoDB connected......")
+});
+
 
 app.listen(8080, () => {
     console.log("Server is running.....");
